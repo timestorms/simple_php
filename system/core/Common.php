48dbb6e3b8fe6ca94e8f7bb3778b4d4798e6dcef
timestorms <764310576@qq.com>
2018-08-23 22:02:19 +0800

    common.php

--- system/core/Common.php
+++ system/core/Common.php
@@ -59,7 +59,49 @@
             $_config_items[$item] = $configs[$item];
             return $_config_items[$item];
         }
-    } 
+    }
+
+    if (!function_exists('get_config')) {
+        function &get_config($replace = []) {
+            static $_configs;
+            if (isset($_configs)) {
+                return $_configs[0];
+            }
+            if (!defined('ENVIRONMENT') || !file_exists(APPPATH . 'config/' . ENVIRONMENT . '/config.php')) {
+                $file_path = APPPATH . 'config/config.php';
+            }
+            if (!file_exists($file_path)) {
+                exit('The configuration file not exist');
+            }
+            require($file_path);
+
+            if (!isset($config) || !is_array($config)) {
+                exit('Your config file does not appear to be formatted corrently');
+            }
+
+            // 如果需要替换
+            if (count($replace) > 0) {
+                foreach ($replace as $key => $val) {
+                    if (isset($config[$key])) {
+                        $config[$key] = $val;
+                    }
+                }
+            }
+
+            $_configs[0] = & $config;
+            return $_config[0];
+        }
+    }
+
+    if (!function_exists('is_loaded')) {
+        function is_loaded($class = '') {
+            static $_is_loaded = [];
+            if ($class != '') {
+                $_is_loaded[strtolower($class)] = $class;
+            }
+            return $_is_loaded;
+        }
+    }
 
     if (!function_exists('_exception_handler')) {
         function __exception_handler($serverity, $message, $filepath, $line) {
